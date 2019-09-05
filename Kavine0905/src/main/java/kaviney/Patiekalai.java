c49d850e0e0da81005d02f7936928a3d2729551a
Aurume <aurelija35@gmail.com>
2019-09-05 13:25:17 +0300

    sutvarkyta, kad salinti butu disabled, bet salina anksciau nei ispeja

---
+++ Kavine0905/src/main/java/kaviney/Patiekalai.java
@@ -0,0 +1,89 @@
+package kaviney;
+
+import javax.persistence.*;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+
+import java.util.List;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Patiekalai {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    private String pav;
+
+    private Integer trukme_ruosimo;
+    
+    private Integer trukme_kaitinimo;    
+    
+    private Double kaina;
+    
+    @OneToMany(mappedBy = "patiekalai",cascade = CascadeType.ALL)
+    private List<Patiekalu_produktai> patiekalu_produktai;
+
+    @OneToMany(mappedBy = "patiekalai",cascade = CascadeType.ALL)
+    private List<Uzsakymai> uzsakymai;    // cia pridejus sujungia patiekalus su uzsakymais.
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPav() {
+		return pav;
+	}
+
+	public void setPav(String pav) {
+		this.pav = pav;
+	}
+
+	public Integer getTrukme_ruosimo() {
+		return trukme_ruosimo;
+	}
+
+	public void setTrukme_ruosimo(Integer trukme_ruosimo) {
+		this.trukme_ruosimo = trukme_ruosimo;
+	}
+
+	public Integer getTrukme_kaitinimo() {
+		return trukme_kaitinimo;
+	}
+
+	public void setTrukme_kaitinimo(Integer trukme_kaitinimo) {
+		this.trukme_kaitinimo = trukme_kaitinimo;
+	}	
+	
+	public Double getKaina() {
+		return kaina;
+	}
+
+	public void setKaina(Double kaina) {
+		this.kaina = kaina;
+	}
+	
+	public List<Uzsakymai> getUzsakymai() {
+		
+		return uzsakymai;
+	}
+	public void setUzsakymai (List<Uzsakymai> uzsakymai) {
+		
+		this.uzsakymai = uzsakymai;
+	}	
+	
+	
+	public List<Patiekalu_produktai> getPatiekalu_produktai() {
+		
+		return patiekalu_produktai;
+	}
+	public void setPatiekalu_produktai (List<Patiekalu_produktai> patiekalu_produktai) {
+		
+		this.patiekalu_produktai = patiekalu_produktai;
+	}
+}
